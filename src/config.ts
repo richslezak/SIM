8315e2ae35a69cd0d6217453a61b2f089521b130
richslezak <richslezak@gmail.com>
2023-01-18 21:46:30 -0800

    setup site data config file. using css theme vars and nav update

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Sound in Motion";
+export const SITE_DESCRIPTION =
+  "Sound in Motion is Boston best car audio, mobile electronics installers specializing in custom high-end car stereo system installation and fabrication.";
+export const SITE_KEYWORDS =
+  "Sound in Motion, car audio, car stereo, mobile electronics, mobile entertainment, mobile video, iPod integration, iPod, xenon headlight theft, HID, xenon, radar detectors, car alarms, auto security, remote start, satellite radio, xm radio, car navigation, GPS, auto electronics installers, sirius radio, HD radio, laser defuser, laser jammer, bluetooth, bluetooth phone, hands free phone, high end car stereo, high end car audio, custom car stereo installation, rear seat entertainment, sound quality, window tinting, mp3, custom dashboards, bmw stereo, porsche stereo, ferrari stereo, audi stereo, car computer, gps tracking, reverse camera, back up camera";
+
+// THEME COLORS
+export const SIM_RED = "#bd0000";
+export const SIM_RED_SECONDARY = "hsla(0, 0%, 100%, 0.3)";
+export const SIM_BLUE = "#3d9ee3";
+export const SIM_WHITE = "#fff";
+export const SIM_WHITE_SECONDARY = "#f6f6f6";
+export const SIM_GRAY = "#5a5a5a";
+export const SIM_BLACK = "#333";
