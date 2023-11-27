2fbcb137427b6aa044fdcb64c59c57950081d792
richslezak <richslezak@gmail.com>
2023-11-27 17:24:21 -0500

    update top favicons

--- src/config.ts
+++ src/config.ts
@@ -6,3 +6,5 @@ export const SITE_DESCRIPTION =
   'Sound in Motion is Boston best car audio, mobile electronics installers specializing in custom high-end car stereo system installation and fabrication.';
 export const SITE_KEYWORDS =
   'Sound in Motion, car audio, car stereo, mobile electronics, mobile entertainment, mobile video, iPod integration, iPod, xenon headlight theft, HID, xenon, radar detectors, car alarms, auto security, remote start, satellite radio, xm radio, car navigation, GPS, auto electronics installers, sirius radio, HD radio, laser defuser, laser jammer, bluetooth, bluetooth phone, hands free phone, high end car stereo, high end car audio, custom car stereo installation, rear seat entertainment, sound quality, window tinting, mp3, custom dashboards, bmw stereo, porsche stereo, ferrari stereo, audi stereo, car computer, gps tracking, reverse camera, back up camera';
+export const SITE_AUTHOR = 'Richard Slezak';
+export const SITE_URL = 'https://www.sound-in-motion.com/';
